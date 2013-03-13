d99c34319264238be3cda9f33be4b06b09b09af0
joshp123 <joshp123@users.noreply.github.com>
2013-03-13 20:25:16 +0000

    ~initial commit~

---
+++ ODESolver/Tweeter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TweetSharp;
+using System.Diagnostics;
+
+namespace ODESolver
+{
+    class Tweeter
+    {
+        private TwitterService service;
+        
+        public Tweeter()
+        {
+            var _consumerKey = "J9iaXHK7R0bpKiRabxQ";
+            var _consumerSecret = "Xs6XJggEvXtMVDvaYAvTLnIm13G70ymcdhwQ3J1M";
+
+            // this authentication snippet borrowed from https://github.com/danielcrenna/tweetsharp
+
+            service = new TwitterService(_consumerKey, _consumerSecret);
+
+            // Step 1 - Retrieve an OAuth Request Token
+            OAuthRequestToken requestToken = service.GetRequestToken();
+
+            // Step 2 - Redirect to the OAuth Authorization URL
+            // Uri uri = service.GetAuthorizationUri(requestToken);
+            // Process.Start(uri.ToString());
+
+            // Step 3 - Exchange the Request Token for an Access Token
+            
+            // OAuthAccessToken access = service.GetAccessToken(requestToken, verifier);
+            // OAuthAccessToken generated manually through dev.twitter.com so no need for this bit since it's just me tweetin ma shit
+
+            var access = new OAuthAccessToken();
+            access.Token = "1196853272-8dPGl6g8dKA4rHrimyerE2WcJBSTKalVRngd4FN";
+            access.TokenSecret = "0SJPjmnexaE5HwOPpLwxvqnrqMG3t0XQrTsH21d0WE";
+
+            // Step 4 - User authenticates using the Access Token
+            service.AuthenticateWith(access.Token, access.TokenSecret);
+            // service.SendTweet(new SendTweetOptions { Status = "Hello World" });
+
+        }
+
+        public TwitterResponse SendTweet(string tweet)
+        {
+            service.SendTweet(new SendTweetOptions { Status = tweet });
+            return service.Response;
+        }
+    }
+}
---
+++ ODESolver/myClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ODESolver
+{
+    /// <summary>
+    /// Quick class to populate my bindingList
+    /// </summary>
+    class partOneA
+    {
+        public double StepSize { get; set; }
+        public double Euler { get; set; }
+        public double ErrorOverStepSize { get; set; }
+        public double RK2 { get; set; }
+        public double ErrorOverStepSizeSquared { get; set; }
+        public double RK4 { get; set; }
+        public double ErrorOverActual { get; set; }
+        public double Actual { get; set; }
+
+
+        public partOneA(double stepSize, double euler, double eulerError, double RK2, double RK2Error, double RK4, double RK4Error, 
+            double Actual)
+        {
+            this.Euler = euler;
+            this.ErrorOverStepSize = eulerError;
+            this.RK2 = RK2;
+            this.ErrorOverStepSizeSquared = RK2Error;
+            this.RK4 = RK4;
+            this.ErrorOverActual = RK4Error;
+            this.Actual = Actual;
+            this.StepSize = stepSize;
+        }
+    }
+
+    /// <summary>
+    /// quick class to populate another silly list
+    /// </summary>
+    class partOneB
+    {
+        public double x { get; set; }
+        public double EulerPointOne{ get; set; }
+        public double EulerPointZeroTwo { get; set;}
+        public double EulerPointZeroZeroFive { get; set; }
+
+        public partOneB(double x, double EulerPointOne, double EulerPointZeroTwo, double EulerPointZeroZeroFive)
+        {
+            this.x = x;
+            this.EulerPointOne = EulerPointOne;
+            this.EulerPointZeroTwo = EulerPointZeroTwo;
+            this.EulerPointZeroZeroFive = EulerPointZeroZeroFive;
+        }
+    }
+
+    /// <summary>
+    /// quick class for prob 2
+    /// </summary>
+    class problem2Class
+    {
+        public double butts;
+    }
+}
